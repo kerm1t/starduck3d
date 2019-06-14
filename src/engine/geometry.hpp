c713f632b15374a6d3f696383ba0322fac4d7d1a
kerm1t <hagenx@gmx.de>
2019-06-14 05:53:17 -0700

    OBB (needed?)

--- src/engine/geometry.hpp
+++ src/engine/geometry.hpp
@@ -9,3 +9,11 @@ struct s_AABB // axis aligned BBox
   float w; // AUTOSAR: h ... 2do: fix!!
   float l;
 };
+
+struct s_OBB // oriented BBox
+{
+  glm::vec3 pos;
+  glm::mat4x4 rotMat;
+  glm::vec3 min_point;
+  glm::vec3 max_point;
+};
