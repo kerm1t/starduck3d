6c0d2359c89fed38f4783758a04087f0c2abd985
kerm1t <hagenx@gmx.de>
2019-07-02 20:53:38 -0700

    OpenGL load texture to a given texture_id

--- src/engine/img_any.cpp
+++ src/engine/img_any.cpp
@@ -44,3 +44,33 @@ GLuint CIMGLoader::loadIMG(const char * imagepath, bool bTransparent)
 
   return textureID;
 };
+
+void CIMGLoader::loadIMG_Tex_ID(const GLuint textureID, const char * imagepath, bool bTransparent)
+{
+  int w;
+  int h;
+  int chan;
+  unsigned char* data;
+  data = stbi_load(imagepath, &w, &h, &chan, 0);
+  assert(data != NULL); // e.g. file not found
+
+  // tut's net ->  stbi__vertically_flip_on_load = true; // 2019-03-03, uv texture vertices upside down!
+
+  // Create one OpenGL texture
+
+  // "Bind" the newly created texture : all future texture functions will modify this texture
+  glBindTexture(GL_TEXTURE_2D, textureID);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+
+  GLenum err = glGetError();
+
+  // Give the image to OpenGL
+  if (bTransparent)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); // tut's ??
+  else
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data); // tut's ??
+
+  free(data);
+};
