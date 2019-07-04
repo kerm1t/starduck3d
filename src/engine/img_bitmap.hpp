1e72892783cc902ea99ef6ef0435991e715f42f4
kerm1t <hagenx@gmx.de>
2019-07-03 20:31:00 -0700

    restructure bmp_loader

--- src/engine/img_bitmap.hpp
+++ src/engine/img_bitmap.hpp
@@ -13,19 +13,24 @@
 
 #include <stdio.h>
 
+struct s_bmp
+{
+  unsigned char header[54];  // Each BMP file begins by a 54-bytes header
+  unsigned int  dataPos;     // Position in the file where the actual data begins
+  unsigned int  width, height;
+  unsigned int  imageSize;   // = width*height*3
+                             // Actual RGB data
+  unsigned char * data;
+};
+
 class CBMPLoader
 {
 public:
-  GLuint loadBMP_custom(const char * imagepath)
+  // return size in bytes
+  size_t loadBMP(const char * imagepath, s_bmp & bmp)
   {
     // Data read from the header of the BMP file
-    unsigned char header[54]; // Each BMP file begins by a 54-bytes header
-    unsigned int dataPos;     // Position in the file where the actual data begins
-    unsigned int width, height;
-    unsigned int imageSize;   // = width*height*3
-    // Actual RGB data
-    unsigned char * data;
-    size_t result;
+    size_t size;
 
     // Open the file
     FILE * file = fopen(imagepath, "rb");
@@ -36,36 +41,45 @@ public:
       return 0;
     }
 
-    if (fread(header, 1, 54, file) != 54) // If not 54 bytes read : problem
+    if (fread(bmp.header, 1, 54, file) != 54) // If not 54 bytes read : problem
     {
       printf("Not a correct BMP file\n");
       return false;
     }
 
-    if (header[0] != 'B' || header[1] != 'M')
+    if (bmp.header[0] != 'B' || bmp.header[1] != 'M')
     {
       printf("Not a correct BMP file\n");
       return 0;
     }
 
     // Read ints from the byte array
-    dataPos   = *(int*)&(header[0x0A]);
-    imageSize = *(int*)&(header[0x22]);
-    width     = *(int*)&(header[0x12]);
-    height    = *(int*)&(header[0x16]);
+    bmp.dataPos   = *(int*)&(bmp.header[0x0A]);
+    bmp.imageSize = *(int*)&(bmp.header[0x22]);
+    bmp.width     = *(int*)&(bmp.header[0x12]);
+    bmp.height    = *(int*)&(bmp.header[0x16]);
 
     // Some BMP files are misformatted, guess missing information
-    if (imageSize == 0) imageSize = width*height * 3; // 3 : one byte for each Red, Green and Blue component
-    if (dataPos == 0)   dataPos = 54; // The BMP header is done that way
+    if (bmp.imageSize == 0) bmp.imageSize = bmp.width * bmp.height * 3; // 3 : one byte for each Red, Green and Blue component
+    if (bmp.dataPos == 0)   bmp.dataPos = 54; // The BMP header is done that way
 
     // Create a buffer
-    data = new unsigned char[imageSize];
+    bmp.data = new unsigned char[bmp.imageSize];
 
     // Read the actual data from the file into the buffer
-    result = fread(data, 1, imageSize, file);
+    size = fread(bmp.data, 1, bmp.imageSize, file);
 
     // Everything is in memory now, the file can be closed
     fclose(file);
+
+    return size;
+  }
+
+  GLuint loadBMP_texID(const char * imagepath)
+  {
+    s_bmp bmp;
+
+    size_t result = loadBMP(imagepath, bmp);
 
     // Create one OpenGL texture
     GLuint textureID;
@@ -78,9 +92,9 @@ public:
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
     // Give the image to OpenGL
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data); // hier gibt es Schwierigkeiten mit .bmp,
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, bmp.width, bmp.height, 0, GL_BGR, GL_UNSIGNED_BYTE, bmp.data);
 
-    delete data;
+    delete bmp.data;
 
     return textureID;
   }
