33987bf0fa1274f0e7aa54e0ebd583fa7e1a2bf1
kerm1t <wolfgang.schulz@gmail.com>
2022-01-07 17:57:25 +0100

    mindmap ... get ready for re-structure

---
+++ devlog/starduck.mm
@@ -0,0 +1,35 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1641574178423" ID="ID_1162147495" MODIFIED="1641574197307" TEXT="starduck">
+<node CREATED="1641574212162" ID="ID_1725651945" MODIFIED="1641574216752" POSITION="right" TEXT="shaderman">
+<node CREATED="1641574276871" ID="ID_1670545685" MODIFIED="1641574490950" TEXT="initshader1">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      standard
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1641574234876" ID="ID_257320488" MODIFIED="1641574242838" TEXT="1 vertex shader"/>
+<node CREATED="1641574249213" ID="ID_420001395" MODIFIED="1641574253667" TEXT="1 fragment shader"/>
+</node>
+<node CREATED="1641574302599" ID="ID_1772745187" MODIFIED="1641574512823" TEXT="initshader2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      help screen
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
